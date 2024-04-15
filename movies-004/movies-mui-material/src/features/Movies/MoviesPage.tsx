7f075fecd48ce6018e1858cd6dfc7966dfc3ba4a
Liudmyla Horobets <127691893+LiudmylaHorobets@users.noreply.github.com>
2024-04-15 19:49:43 +0200

    Add files via upload

---
+++ movies-004/movies-mui-material/src/features/Movies/MoviesPage.tsx
@@ -0,0 +1,56 @@
+import { Movie, fetchMovies } from "../../reducers/movies";
+import { connect } from "react-redux";
+import { RootState } from "../../store";
+import MovieCard from "./MovieCard";
+
+import { useEffect } from "react";
+import { useAppDispatch } from "../../hooks";
+import { Container, Grid, LinearProgress, Typography } from "@mui/material";
+
+interface MoviesProps {
+  movies: Movie[];
+  loading: boolean;
+}
+
+export function MoviesPage({ movies, loading }: MoviesProps) {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+
+  return (
+    <Container sx={{ py: 8 }} maxWidth="lg">
+      <Typography variant="h4" align="center" gutterBottom>
+        Now playing
+      </Typography>
+      {loading ? (
+        <LinearProgress color="secondary" />
+      ) : (
+        <Grid container spacing={4}>
+          {movies.map((m) => (
+            <Grid item key={m.id} xs={12} sm={6} md={4}>
+              <MovieCard
+                key={m.id}
+                id={m.id}
+                title={m.title}
+                overview={m.overview}
+                popularity={m.popularity}
+                image={m.image}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Container>
+  );
+}
+
+const mapStateToProps = (state: RootState) => ({
+  movies: state.movies.top,
+  loading: state.movies.loading,
+});
+
+const connector = connect(mapStateToProps);
+
+export default connector(MoviesPage);
---
+++ movies-004/movies-mui-material/src/reducers/movies.ts
@@ -0,0 +1,68 @@
+import { client } from "../api/tmdb";
+import { ActionWithPayload, createReducer } from "../redux/utils";
+import { AppThunk } from "../store";
+
+export interface Movie {
+  id: number;
+  title: string;
+  popularity: number;
+  overview: string;
+  image?: string;
+}
+
+interface MovieState {
+  top: Movie[];
+  loading: boolean;
+}
+
+const initialState: MovieState = {
+  top: [],
+  loading: false,
+};
+
+const moviesLoaded = (movies: Movie[]) => ({
+  type: "movies/loaded",
+  payload: movies,
+});
+
+const moviesLoading = () => ({
+  type: "movies/loading",
+});
+
+export function fetchMovies(): AppThunk<Promise<void>> {
+  return async (dispatch, getState) => {
+    dispatch(moviesLoading());
+
+    const config = await client.getConfiguration();
+    const imageUrl = config.images.base_url;
+    const results = await client.getNowPlaying();
+
+    const mappedResults: Movie[] = results.map((m) => ({
+      id: m.id,
+      title: m.title,
+      popularity: m.popularity,
+      overview: m.overview,
+      image: m.backdrop_path ? `${imageUrl}w780${m.backdrop_path}` : undefined,
+    }));
+
+    dispatch(moviesLoaded(mappedResults));
+  };
+}
+
+const moviesReducer = createReducer<MovieState>(initialState, {
+  "movies/loaded": (state, action: ActionWithPayload<Movie[]>) => {
+    return {
+      ...state,
+      top: action.payload,
+      loading: false,
+    };
+  },
+  "movies/loading": (state, action) => {
+    return {
+      ...state,
+      loading: true,
+    };
+  },
+});
+
+export default moviesReducer;
